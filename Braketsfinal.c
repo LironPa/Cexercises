c9d1893c3513655a4cb4ef3e1d62f699532cd2f9
LironPa <122447734+LironPa@users.noreply.github.com>
2023-01-30 16:40:01 +0200

    Add files via upload

--- Braketsfinal.c
+++ Braketsfinal.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #pragma warning (disable:4996)
-int Returnnumber(char str[], int index)
+int Stringcalculator(char str[])
 {
-	int   beforeindex, beforenum, afternum, summultiply,p, total = 0;
-	char tempstr[10000];
-
-	for (p = index; p < strlen(str) && str[p] != ')'; p++)
+	int i, j, p, firstTime = 1;
+	int   beforeindex, beforenum, afternum, summultiply, total = 0;
+	char tempstr[10000] = "";
+	for (p = 0; p < strlen(str); p++)
 	{
 		if (str[p] == 'X' || str[p] == '/')
 		{
@@ -36,14 +36,85 @@ int Returnnumber(char str[], int index)
 				tempstr[a + 1] = 0;
 			}
 
-			 return strcpy(str, tempstr);
+			strcpy(str, tempstr);
 
-			
+			p = newindex;
+		}
+
+	}
+	total = 0;
+	for (i = 0; i < strlen(str); i++)
+	{
+		int current = 0;
+		current = findnumber(str, i);
+
+
+		if (firstTime)
+		{
+			total = current;
+			firstTime = 0;
+		}
+		else {
+
+
+			total = str[i - 1] == '+' ? total + current : total;
+			total = str[i - 1] == '-' ? total - current : total;
+
+
 		}
 
 
+
+
+		i = findnextindex(str, i);
 	}
-	
+	return total;
+}
+int ReturnBraketSolved(char str[], int firstindex, int lastindex)
+{
+	int n = 0;
+	int   beforeindex, beforenum, afternum, summultiply,p, total = 0, current = 0,a, ifonlyonce = 1;
+	char tempstr[10000] = "", summultiplystr[1000];
+
+
+	for (p = firstindex; p < lastindex; p++)
+	{
+		
+		if (str[p] == 'X' || str[p] == '/')
+		{
+			
+			beforenum = findnumber(str, firstindex + 1);
+			afternum = findnumber(str, p + 1);
+			summultiply = str[p] == 'X' ? beforenum * afternum : beforenum / afternum;
+			sprintf(summultiplystr, "%d", summultiply);
+			strcat(tempstr, summultiplystr);
+			p = findeprevindex(str, lastindex -1);
+			int newindex = strlen(tempstr);
+			int tmpIndex = newindex;
+			
+			for (a = p; a < lastindex; a++)
+			{
+
+				tempstr[tmpIndex] = str[a];
+				tmpIndex++;
+				tempstr[a + 1] = 0;
+			}
+			
+			beforenum = findnumber(tempstr, n);
+			n = findnextindex(tempstr, n);
+			afternum = findnumber(tempstr, n + 1);
+
+		}
+		if (tempstr[n] == '+' || tempstr[n] == '-') {
+			total = tempstr[n] == '+' ? beforenum + afternum : beforenum - afternum;
+			
+		}
+		if (tempstr[n] == 'X' || tempstr[n] == '/') {
+			total = tempstr[n] == 'X' ? beforenum * afternum : beforenum / afternum;
+		}
+	}
+
+	return total;
 }
 int Findfirstbraket(char str[], int index)
 {
@@ -134,64 +205,26 @@ int main()
 	strcpy(str, "520X2+(1000X2-985)X14+90/10+(850/50+13)-8/2+255+(425X10)-80X50+(457+3)+140000");
 	strcpy(tempstr, "");
 	int i, j, p, b, btemp = 0, a, firstTime = 1, firstbraket =1;
-	int   beforeindex = 0, beforenum = 0, afternum, summultiply = 0;
+	int   beforeindex = 0, beforenum = 0, afternum, summultiply = 0, replace = 0;
 	int braketbeforenum = 0, braketafternum = 0, braketsum = 0, total = 0;;
 	
 	for (b = 0; b < strlen(str); b++)
 	{
 		if (str[b] == '(') {
-			Findlastbraket(str, b);
+			i = Findnextbraket(str, b);
 			btemp = Findlastbraket(str, btemp);
-
+			replace = ReturnBraketSolved(str, i, btemp);
 
 			for (b = b; b < strlen(str) && b < btemp; b++)
 			{
 
-				braketbeforenum = findnumber(str, b + 1);
-				b = findnextindex(str, b + 1);
-				braketafternum = findnumber(str, b + 1);
-				if (firstbraket) {
-					if (str[b] == 'X' || str[b] == '/') {
-						b = Findfirstbraket(str, b) + 1;
-						braketsum = Returnnumber(str, b);
-						braketbeforenum = findnumber(str, b + 1);
-						b = findnextindex(str, b + 1);
-						braketafternum = findnumber(str, b + 1);
-						firstbraket = 0;
-					}
-				}
-
-				else if (str[b] == 'X' || str[b] == '/') 
-				{
-					
-					b = Findfirstbraket(str, b) + 1;
-						braketsum = Returnnumber(str, b);
-						braketsum = str[b] == 'X' ? braketbeforenum * braketafternum : braketbeforenum / braketafternum;
-						braketbeforenum = findnumber(str, b + 1);
-						b = findnextindex(str, b + 1);
-						braketafternum = findnumber(str, b + 1);
-						
-				}
-				if (str[b] == '+' || str[b] == '-') {
-					
-					braketsum = str[b] == '+' ? braketbeforenum + braketafternum : braketbeforenum - braketafternum;
-				}
-				else if (braketafternum == 0) {
-
-				if (str[b] == '(') {
-					braketsum = findnumber(str, b + 1);
-				}
-				}
-				b = btemp;
-				b = Findfirstbraket(str, b) + 1;
-				
 				for (a = 0; a < b; a++)
 				{
 					tempstr[a] = str[a];
 				}
 				tempstr[a] = '\0';
 				char sumbraket[1000];
-				sprintf(sumbraket, "%d", braketsum);
+				sprintf(sumbraket, "%d", replace);
 				strcat(tempstr, sumbraket);
 				b = Findlastbraket(str, b) + 1;
 				
@@ -213,71 +246,48 @@ int main()
 			b = Findnextbraket(str, b) - 1;
 		}
 	}
-
-
-	
-
-	for (p = 0; p < strlen(str); p++)
-	{
-		if (str[p] == 'X' || str[p] == '/')
-		{
-			beforeindex = findeprevindex(str, p - 1);
-			beforenum = findnumber(str, beforeindex + 1);
-			afternum = findnumber(str, p + 1);
-			summultiply = str[p] == 'X' ? beforenum * afternum : beforenum / afternum;
-
-
-			for (a = 0; a < beforeindex + 1; a++)
-			{
-				tempstr[a] = str[a];
-			}
-			tempstr[a] = '\0';
-			char summultiplystr[1000];
-			sprintf(summultiplystr, "%d", summultiply);
-			strcat(tempstr, summultiplystr);
-			p = findnextindex(str, p + 1);
-			int newindex = strlen(tempstr);
-			int tmpIndex = newindex;
-			for (a = p; a < strlen(str); a++)
-			{
-
-				tempstr[tmpIndex] = str[a];
-				tmpIndex++;
-				tempstr[a + 1] = 0;
-			}
-
-			strcpy(str, tempstr);
-
-			p = newindex;
-		}
-	}
+	int Solution = Stringcalculator(str);
 
 	
-	total = 0;
-	for (i = 0; i < strlen(str); i++)
-	{
-		int current = 0;
-		current = findnumber(str, i);
 
-
-		if (firstTime)
-		{
-			total = current;
-			firstTime = 0;
-		}
-		else {
-
-
-			total = str[i - 1] == '+' ? total + current : total;
-			total = str[i - 1] == '-' ? total - current : total;
-
-
-		}
-
-
-
-
-		i = findnextindex(str, i);
-	}
-	printf("the answer is:\n%d", total);
+	printf("the answer is:\n%d", Solution);
 }
+
+//
+//braketbeforenum = findnumber(str, b + 1);
+//b = findnextindex(str, b + 1);
+//braketafternum = findnumber(str, b + 1);
+//if (firstbraket) {
+//	if (str[b] == 'X' || str[b] == '/') {
+//		b = Findfirstbraket(str, b) + 1;
+//
+//		braketbeforenum = findnumber(str, b + 1);
+//		b = findnextindex(str, b + 1);
+//		braketafternum = findnumber(str, b + 1);
+//		firstbraket = 0;
+//	}
+//}
+//
+//else if (str[b] == 'X' || str[b] == '/')
+//{
+//
+//	b = Findfirstbraket(str, b) + 1;
+//
+//	braketsum = str[b] == 'X' ? braketbeforenum * braketafternum : braketbeforenum / braketafternum;
+//	braketbeforenum = findnumber(str, b + 1);
+//	b = findnextindex(str, b + 1);
+//	braketafternum = findnumber(str, b + 1);
+//
+//}
+//if (str[b] == '+' || str[b] == '-') {
+//
+//	braketsum = str[b] == '+' ? braketbeforenum + braketafternum : braketbeforenum - braketafternum;
+//}
+//else if (braketafternum == 0) {
+//
+//	if (str[b] == '(') {
+//		braketsum = findnumber(str, b + 1);
+//	}
+//}
+//b = btemp;
+//b = Findfirstbraket(str, b) + 1;
