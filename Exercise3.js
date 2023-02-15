b7960b4e9ac47e223269ad830482756e2c32134d
LironPa <122447734+LironPa@users.noreply.github.com>
2023-02-15 13:46:57 +0200

    Add files via upload

---
+++ Exercise3.js
@@ -0,0 +1,9 @@
+let name = 'Liron';
+console.log(name);
+let intresetRate = 0.3;
+intresetRate = 1;
+console.log(intresetRate);
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
